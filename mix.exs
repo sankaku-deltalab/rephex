42d3eff921471c13e0b8e5f5bf5a02e1159a3f78
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-12-21 14:41:11 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Rephex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/sankaku-deltalab/rephex",
+      homepage_url: "https://github.com/sankaku-deltalab/rephex",
+      docs: docs()
     ]
   end
 
@@ -22,7 +27,30 @@ defmodule Rephex.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5"},
-      {:phoenix_live_view, "~> 0.20"}
+      {:phoenix_live_view, "~> 0.20"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Redux-toolkit in Phenix LiveView"
+  end
+
+  defp package do
+    [
+      contributors: ["Sankaku <sankaku_dlt.45631@outlook.jp>"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/sankaku-deltalab/rephex"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ]
     ]
   end
 end
