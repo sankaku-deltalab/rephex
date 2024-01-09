f6a1d5cf1d9cbfe1c7355e031db84d69874ee5d5
Sankaku <sankaku_dlt.45631@outlook.jp>
2024-01-09 15:09:40 +0000

    Split Rephex.AsyncAction

--- lib/async_action.ex
+++ lib/async_action.ex
@@ -4,13 +4,12 @@ defmodule Rephex.AsyncAction do
   @callback before_async(socket :: Socket.t(), payload :: map()) ::
               {:continue, Socket.t()} | {:abort, Socket.t()}
   @callback start_async(state :: map(), payload :: map(), send_msg :: (any() -> any())) :: any()
-  @callback resolve(socket :: Socket.t(), result :: {:ok, any()} | {:exit, any()}) :: Socket.t()
-  @callback receive_message(socket :: Socket.t(), message :: any()) :: Socket.t()
   @callback before_cancel(socket :: Socket.t(), reason :: any()) ::
               {:continue, Socket.t()} | {:abort, Socket.t()}
 
   defmacro __using__(_opt \\ []) do
     quote do
+      @behaviour Rephex.AsyncAction.Base
       @behaviour Rephex.AsyncAction
 
       # NOTE: `@type payload`, `@type cancel_reason` must be defined.
@@ -66,44 +65,5 @@ defmodule Rephex.AsyncAction do
       {:abort, %Socket{} = socket} ->
         socket
     end
-  end
-end
-
-defmodule Rephex.AsyncAction.Handler do
-  alias Phoenix.LiveView.Socket
-
-  defmacro __using__(opt) do
-    async_modules = Keyword.fetch!(opt, :async_modules)
-
-    quote do
-      @impl true
-      def handle_info({Rephex.AsyncAction, async_module, _message} = msg, %Socket{} = socket)
-          when is_atom(async_module) and async_module in unquote(async_modules) do
-        Rephex.AsyncAction.Handler.handle_info_by_async_message(msg, socket)
-      end
-
-      @impl true
-      def handle_async(name, async_fun_result, %Socket{} = socket)
-          when name in unquote(async_modules) do
-        Rephex.AsyncAction.Handler.handle_async_action(name, async_fun_result, socket)
-      end
-    end
-  end
-
-  def handle_info_by_async_message(
-        {Rephex.AsyncAction, async_module, message} = _msg,
-        %Socket{} = socket
-      )
-      when is_atom(async_module) do
-    # NOTE: Caller must check `async_module` in async modules.
-    if socket.parent_pid != nil,
-      do: raise("Must not receive message in async on propagated state.")
-
-    {:noreply, async_module.receive_message(socket, message)}
-  end
-
-  def handle_async_action(async_module, async_fun_result, %Socket{} = socket) do
-    # NOTE: Caller must check `async_module` in async modules.
-    {:noreply, async_module.resolve(socket, async_fun_result)}
   end
 end
---
+++ lib/async_action_base.ex
@@ -0,0 +1,53 @@
+defmodule Rephex.AsyncAction.Base do
+  alias Phoenix.LiveView.Socket
+
+  @type result :: any()
+  @type cancel_reason :: any()
+  @type message :: any()
+
+  @callback resolve(
+              socket :: Socket.t(),
+              result :: {:ok, result()} | {:exit, cancel_reason()}
+            ) :: Socket.t()
+
+  @callback receive_message(socket :: Socket.t(), message :: message()) :: Socket.t()
+end
+
+defmodule Rephex.AsyncAction.Handler do
+  alias Phoenix.LiveView.Socket
+
+  defmacro __using__(opt) do
+    async_modules = Keyword.fetch!(opt, :async_modules)
+
+    quote do
+      @impl true
+      def handle_info({Rephex.AsyncAction.Base, async_module, _message} = msg, %Socket{} = socket)
+          when is_atom(async_module) and async_module in unquote(async_modules) do
+        Rephex.AsyncAction.Handler.handle_info_by_async_message(msg, socket)
+      end
+
+      @impl true
+      def handle_async(name, async_fun_result, %Socket{} = socket)
+          when name in unquote(async_modules) do
+        Rephex.AsyncAction.Handler.handle_async_action(name, async_fun_result, socket)
+      end
+    end
+  end
+
+  def handle_info_by_async_message(
+        {Rephex.AsyncAction.Base, async_module, message} = _msg,
+        %Socket{} = socket
+      )
+      when is_atom(async_module) do
+    # NOTE: Caller must check `async_module` in async modules.
+    if socket.parent_pid != nil,
+      do: raise("Must not receive message in async on propagated state.")
+
+    {:noreply, async_module.receive_message(socket, message)}
+  end
+
+  def handle_async_action(async_module, async_fun_result, %Socket{} = socket) do
+    # NOTE: Caller must check `async_module` in async modules.
+    {:noreply, async_module.resolve(socket, async_fun_result)}
+  end
+end
